9278f57056a5f5868aa70f2621c0329229fd318f
NOE CAMAVILCA <noejhon90@gmail.com>
2021-03-13 00:37:06 -0500

    actualizando la fecha

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... v12032021-2";
+            return "Net Core 5.0 Running ... v13032021-3";
         }
     }
 }
